5b27d4f36a0660b2f24bf0de8759eaf11dea2c4a
Enes Yıldırım <enes5519@gmail.com>
2021-12-04 00:35:42 +0300

    feat: basic website

---
+++ src/pages/index.tsx
@@ -0,0 +1,36 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => (
+  <div className="container">
+    <Head>
+      <title>Enes Yıldırım | Kişisel Blog</title>
+      <meta charSet="UTF-8" />
+      <meta name="description" content="Enes Yıldırım'ın öğretici ve eğlenceli blogları." />
+      <meta name="keywords" content="Developer, Frontend, Psikoloji, Geliştirici, Linux" />
+      <meta name="author" content="Enes Yıldırım" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    </Head>
+    <div className="c-small flex-center px-5 py-4 w-full">
+      <a className="btn btn-ghost font-normal" href="/">
+        Anasayfa
+      </a>
+      <a className="btn btn-ghost font-normal" href="/">
+        Blog
+      </a>
+      <a className="btn btn-ghost font-normal" href="/">
+        Özgeçmiş
+      </a>
+    </div>
+    <div className="c-small py-16">
+      <h1>Hoşgeldiniz, ben Enes</h1>
+      <p className="mt-6">Front-end teknolojileri, linux, psikoloji gibi konularla yakından ilgileniyorum.</p>
+    </div>
+    <div className="c-small py-16">
+      <h2>Son Bloglarım</h2>
+      <p className="mt-6">Beklemede kalın...</p>
+    </div>
+  </div>
+);
+
+export default Home;
---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  mode: "jit",
+  purge: ["./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    container: {
+      center: true,
+      padding: "1rem",
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+      },
+    },
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      white: "#fff",
+      grey: {
+        100: "#F2F2F7",
+        400: "#AEAEB2",
+        DEFAULT: "#8E8E93",
+        900: "#1C1C1E",
+      },
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
