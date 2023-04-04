d0528cf11c71578f4924a8939cbd518d585e3b89
Enes Yıldırım <enes5519@gmail.com>
2023-04-04 23:47:14 +0300

    style: add indicator for routing

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,29 @@
-<script>
+<script lang="ts">
 	import 'prismjs/themes/prism-twilight.css';
+	import 'nprogress/nprogress.css';
 	import Header from '$lib/components/organisms/Header.svelte';
+	import { navigating } from '$app/stores';
+	import NProgress from 'nprogress';
+
+	NProgress.configure({ showSpinner: false });
+
+	$: {
+		if ($navigating) {
+			console.log('KORRR');
+			NProgress.start();
+		} else {
+			NProgress.done();
+		}
+	}
 </script>
 
 <Header />
 <main>
 	<slot />
 </main>
+
+<style>
+	:global(#nprogress .bar) {
+		background: var(--color-amber);
+	}
+</style>
